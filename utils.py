7c15d181159e7b1c01219e5101a9c536e97959ac
nov <nov@fsc.chubu.ac.jp>
2025-06-17 12:03:42 +0900

    init repo

---
+++ utils.py
@@ -0,0 +1,17 @@
+# %%
+
+import geopandas as gpd
+import numpy as np
+shapefile_path = "./data/bus_kasugai.shp"
+def read_shapefile(file_path):
+    gdf = gpd.read_file(file_path, encoding='shift_jis')
+    gdf['normal_dist'] = np.random.normal(loc=0, scale=1, size=len(gdf))    
+    new_gdf = gdf[gdf["normal_dist"] > 0]
+    print(f"Read {len(gdf)} features from {file_path}")
+    print(f"CRS: {gdf.crs}")
+    print("columns: ",gdf.columns.tolist())
+
+
+read_shapefile(shapefile_path) 
+
+# %%
