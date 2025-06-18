b5ae49f75013a33f2e0719c838e55f0335c859e2
nov <nov@fsc.chubu.ac.jp>
2025-06-18 09:47:20 +0900

    edit del wat4

--- utils.py
+++ utils.py
@@ -2,9 +2,11 @@
 
 import geopandas as gpd
 import numpy as np
-shapefile_path = "./data/bus_kasugai.shp"
+#shapefile_path = "./data/bus_kasugai.shp"
+shapefile_path = "./data/bus.geojson"
+
 def read_shapefile(file_path):
-    gdf = gpd.read_file(file_path, encoding='shift_jis')
+    gdf = gpd.read_file(file_path, encoding='UTF-8', errors="replace")
     gdf['normal_dist'] = np.random.normal(loc=0, scale=1, size=len(gdf))    
     new_gdf = gdf[gdf["normal_dist"] > 0]
     print(f"Read {len(gdf)} features from {file_path}")
